1dc08731d8c188a9f1b68a9c514cd5c344c7532f
karanVerma1107 <157015568+karanVerma1107@users.noreply.github.com>
2025-01-11 09:33:49 +0530

    kkk

--- index.js
+++ index.js
@@ -15,10 +15,14 @@ dotenv.config({ path: 'o.env' });
 const app = express();
 const port = process.env.PORT || 3000;
 
+{/*
 app.use(cors({
   origin: 'https://navbharatniwas.in', // Your Netlify site
   credentials: true, // Allow credentials (cookies) to be sent
 }));
+*/}
+app.use(cors());
+
 app.use(express.json());
 
 app.use(cookieParser());

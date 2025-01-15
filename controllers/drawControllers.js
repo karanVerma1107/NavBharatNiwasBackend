ea55970e210d5e0d12fffde25bc3caa6f248fc1a
karanVerma1107 <157015568+karanVerma1107@users.noreply.github.com>
2025-01-15 21:40:37 +0530

    98980

--- controllers/drawControllers.js
+++ controllers/drawControllers.js
@@ -170,7 +170,7 @@ const text = `
   
   <!-- Add the image with src="https://navbharatniwasbackend.onrender.com/uploads/images/trans.png", width="154vmax" height="126.2vmax" margin 2vmax 0.5vmax -->
   <div style="margin: 2vmax 0.5vmax;">
-    <img src="https://navbharatniwasbackend.onrender.com/uploads/images/trans.png" alt="Lucky Draw" width="194vmax" height="126.3vmax" style="margin: 2vmax 0.5vmax;"/>
+    <img src="https://navbharatniwasbackend.onrender.com/uploads/images/oo.jpg" alt="Lucky Draw" width="194vmax" height="126.3vmax" style="margin: 2vmax 0.5vmax;"/>
   </div>
 
   <p style="margin-top: 30px;">
@@ -451,7 +451,7 @@ export const updateLuckyDrawStatus = asyncHandler(async (req, res, next) => {
                    In the event that you are not allotted a prize, we want to assure you that the full amount you contributed to the Lucky Draw will be refunded.
                </p>
 
-               <img src="https://navbharatniwasbackend.onrender.com/uploads/images/trans.png" width="194vmax" height="126.2vmax" style="margin: 2vmax 0.5vmax;" alt="Lucky Draw Image">
+               <img src="https://navbharatniwasbackend.onrender.com/uploads/images/00.jpg" width="194vmax" height="126.2vmax" style="margin: 2vmax 0.5vmax;" alt="Lucky Draw Image">
 
                <p>Thank you for participating!</p>
                <p><b>LINK:</b> <a href="https://navbharatniwas.in/draw/${luckyDraw._id}" style="color: #3498db; text-decoration: none; font-size: 1.2vmax;">Click here to view your application</a></p>

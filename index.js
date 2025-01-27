56af070b566f3b2645e30c5faac6678f22154c00
karanVerma1107 <157015568+karanVerma1107@users.noreply.github.com>
2025-01-28 05:09:42 +0530

    ios212

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ import cookieParser from 'cookie-parser';
 import connectDB from './connectionDB.js';
 import errorHandler from './middleware/errorHandler.js';
 import { fileURLToPath } from 'url';
+import fs from 'fs';
 import path from 'path';
-import session from 'express-session';
+import https from 'https';
 
 // Load environment variables from .env file
-dotenv.config({ path: 'o.env' });
+dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3008;  // Default port to 3008, can be adjusted
 
 // CORS configuration to only accept requests from https://www.navbharatniwas.in
 app.use(cors({
@@ -21,14 +22,6 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://www.navbharatniwas.in'); // Dynamically set the origin from the request header
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  next();
-});
 
 // Middleware to parse JSON bodies
 app.use(express.json());
@@ -62,7 +55,13 @@ app.use('/api/v1', Srouter);
 // Error handler middleware
 app.use(errorHandler);
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+// HTTPS server setup
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/navbharatniwas.in/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/navbharatniwas.in/fullchain.pem'),
+};
+
+// Create an HTTPS server
+https.createServer(options, app).listen(port, () => {
+  console.log(`Server is running on https://localhost:${port}`);
 });
